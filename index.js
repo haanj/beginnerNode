b2be830e0a4946b008b48eff9aa55e9f5b69c86f
haanj <joshua.haan@ymail.com>
2016-03-02 12:41:23 -0800

    final commit, done reading

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var server = require(__dirname + '/server');
+var router = require(__dirname + '/router');
+var requestHandlers = require(__dirname + '/requestHandlers');
 
-server.start();
+var handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+handle['/show'] = requestHandlers.show;
+
+server.start(router.route, handle);
